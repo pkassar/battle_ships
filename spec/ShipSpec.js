b67cfc1abca620cd657d69a94ba999281a0118d7
Paul Kassar <paulkassar@gmail.com>
2017-10-10 14:20:40 +0100

    adding error test for limit of board

--- spec/ShipSpec.js
+++ spec/ShipSpec.js
@@ -15,4 +15,8 @@ describe('Ship', function() {
     ship = new Ship('B2', 'v', 4);
     expect(ship.position).toEqual(['B2', 'B3', 'B4', 'B5']);
   })
+
+  it('size of our board',function(){
+    expect( function() {new Ship ('K1', 'v', 3)}).toThrow(new Error('Ship outside the board'))
+  })
 })
