01f9c3bff8ce309e33cbe0d7592faf31f3fc60c1
Paul Kassar <paulkassar@gmail.com>
2017-10-10 16:34:14 +0100

    overlapping ships

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,8 +2,12 @@ describe('game', function() {
   var game = new Game();
 
   it('fits on board',function(){
-    expect( function() { game.placeShip ('J8', 'v', 3) }).not.toThrow(new Error("Ship outside the board"))
-    expect(game.ships[0].position).toEqual(['J8', 'J9', 'J10'])
+    expect( function() { game.placeShip ('J7', 'v', 3) }).not.toThrow(new Error("Ship overlapping"))
+    expect(game.ships[0].position).toEqual(['J7', 'J8', 'J9'])
+  })
+
+  it('Overlapps with other ship already placed',function(){
+    expect( function() { game.placeShip ('I8', 'h', 2) }).toThrow(new Error("Ship overlapping"))
   })
 
   it('doesnt fit rows',function(){
--- src/Game.js
+++ src/Game.js
@@ -4,16 +4,30 @@ function Game() {
 
 Game.prototype.placeShip = function (anchor, orientation , size) {
   checkBoundary(anchor, orientation, size);
-  this.ships.push(new Ship(anchor, orientation , size))
+  var ship = new Ship(anchor, orientation , size)
+  this.checkOverlap(ship)
+  this.ships.push(ship)
+}
+
+Game.prototype.checkOverlap = function(ship1){
+  var duplicates = this.flatten().filter(function(val) {
+  return ship1.position.indexOf(val) != -1;
+  });
+  console.log(duplicates)
+  if (duplicates.length != 0 ) throw Error("Ship overlapping")
+}
+
+Game.prototype.flatten = function (){
+  return [].concat.apply([], this.ships.map(function(ship){ return ship.position }))
 }
 
 checkBoundary = function(anchor, orientation, size){
   if (orientation == 'h'){
-    if (parseInt(anchor.split('')[1]) > 11  ||
+    if (parseInt(anchor.split('')[1]) > 10  ||
       (anchor.split('')[0].charCodeAt() + size) > 76
     ) throw Error("Ship outside the board")
   }else{
-    if (parseInt(anchor.split('')[1]) + size > 11 ||
+    if (parseInt(anchor.split('')[1]) + size > 10 ||
       (anchor.split('')[0].charCodeAt()) > 74
     ) throw Error("Ship outside the board")
   }
